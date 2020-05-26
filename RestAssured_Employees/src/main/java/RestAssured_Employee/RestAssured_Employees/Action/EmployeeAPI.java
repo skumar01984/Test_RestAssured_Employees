68eaea5e148964cccb98e4076d441d7218c3ebdd
sunil.kumar <sunil.kumar@visiblealpha.com>
2020-05-26 08:42:04 +0530

    Adding Test Code

--- RestAssured_Employees/src/main/java/RestAssured_Employee/RestAssured_Employees/Action/EmployeeAPI.java
+++ RestAssured_Employees/src/main/java/RestAssured_Employee/RestAssured_Employees/Action/EmployeeAPI.java
@@ -1,6 +1,5 @@
 package RestAssured_Employee.RestAssured_Employees.Action;
 
-
 import org.json.simple.JSONObject;
 //import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
@@ -17,67 +16,110 @@ import java.util.Map;
 import junit.framework.Assert;
 
 import static io.restassured.RestAssured.*;
+import static org.testng.Assert.assertEquals;
 
-public class EmployeeAPI extends Config{
-	
+public class EmployeeAPI extends Config {
+
 	Response response;
-	String[] text2 = null;
+	String[] json = null;
 	JsonPath jsonPath = null;
-	
+	List<Map<Object, Object>> employeeList = null;
+
 	public EmployeeAPI() {
-		response=given().contentType(ContentType.TEXT).get(Config.employeeURL);
-		String text = response.
-				then()
-				.statusCode(200)
-				.extract()
-				.body()
-				.asString();
-		
-		String[] text1 = text.replace("},", "}")
-				.replace("....", "")
-				.split("<pre>");
-		text2 = text1[1].split("</pre>");
-		System.out.println("================================================================== \n"+text2[0]);
-		
-	
+		response = given().contentType(ContentType.TEXT).get(Config.employeeURL);
+		String text = response.then().statusCode(200).extract().body().asString();
+
+		// String[] extractJson = text.replace("},", "}").replace("....",
+		// "").split("<pre>");
+		String[] extractJson = text.replace("....", "{}").split("<pre>");
+		json = extractJson[1].split("</pre>");
+		System.out.println("=================================== \n" + json[0]);
+
 	}
-	
-	@Test(priority=0)
+
+	@Test(priority = 0)
+	public void CheckTotalNumberOfEmployees() {
+		try {
+			jsonPath = new JsonPath(json[0]);
+
+			// First Way : to get number of employee from list
+
+			int numFromsResponse = jsonPath.getInt("data.id.size()");
+			System.out.println("1 way using Json Parser: Total no. of employee : " + numFromsResponse);
+
+			// Second Way : to get number of employee from list
+
+			employeeList = jsonPath.getList("data");
+			System.out.println("2 way using List: Total no. of employee : " + employeeList.size());
+
+			assertEquals(employeeList.size(), 2);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test(priority = 1)
 	public void getEmployeeID() {
 		try {
+
+			// To get the employee data id from list
+
+			jsonPath = new JsonPath(json[0]);
 			
-			// To get the data list id
-			System.out.println(text2[0]);
-			jsonPath = new JsonPath(text2[0]);
-		    String id = jsonPath.getString("data.id[0]"); // To get employee id from list
-		    //String data=jsonPath.getString("data");
-		    System.out.println("Employaee id is : "+id);
-		
-	}catch(Exception e) {
-		e.printStackTrace();
+			// 1. Way : To get the second employee id using json parser
+
+			String id = jsonPath.getString("data.id[1]");
+
+			// Second employee data is not present in the response text.
+			// Extracted Json will return null
+
+			if (id == null) {
+				System.out.println("Second employee data is not present in the response text." + id);
+			} else {
+				System.out.println("Second employee data is present in the response text." + id);
+			}
+
+			System.out.println("=======================================================================");
+			// assertEquals(id, null);
+
+			// 2. Way using Has Map : To get the second employee id using Has Map
+			if (employeeList.size() > 1) {
+
+				/*
+				 * employeeList.forEach(empData -> { System.out.println("Employee Data is : " +
+				 * empData); });
+				 */
+				String empkey = null, empValue = null;
+				for (int i = 0; i < employeeList.size(); i++) {
+
+					Map<Object, Object> map = employeeList.get(i);
+					if (map.containsKey("id")) {
+						for (Map.Entry<Object, Object> entry : map.entrySet()) {
+							empkey = entry.getKey().toString();
+							empValue = entry.getValue().toString();
+							// System.out.println("key: " + empkey + " value: " + empValue);
+
+							if (empkey.equals("id")) {
+								System.out.println(
+										"First employee id is present in the JSON. as Key : " + empkey + " ==> " + empValue);
+								System.out.println("=================================================================");
+							}
+
+						}
+
+					} else {
+						System.out.println("Second employee data is not present in the JSON. as Key : " + empkey
+								+ " ==> " + empValue);
+						System.out.println("=======================================================================");
+					}
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-		
-	
-	@Test(priority=1)
-	public void CheckTotalSizeOfEmployee() {
-		try {
-		jsonPath = new JsonPath(text2[0]);	
-		
-		// First Way to get size of list
-		int numFromsResponse = jsonPath.getInt("data.id.size()");
-		System.out.println("Employee size of list 1.way : "+ numFromsResponse);
-		
-		// Second Way to get size of list
-		List<Map<Object, Object>> employeeList = jsonPath.getList("data");
-	    System.out.println("Employee size of list 1.way : " + employeeList.size());
-		
-		
-	}catch(Exception e) {
-		e.printStackTrace();
-		}
-	}
-	
-	
-	
+
 }
